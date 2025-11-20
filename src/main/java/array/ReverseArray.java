0af3665e4f1031c0976b517ab61626812d77b43e
thangvu <thangducvu777@gmail.com>
2025-11-20 15:41:50 -0600

    adding reversearray

--- src/main/java/array/ReverseArray.java
+++ src/main/java/array/ReverseArray.java
@@ -3,19 +3,15 @@ package array;
 import java.util.Arrays;
 
 // https://leetcode.com/problems/rotate-array/
-// Time : O(N) Runs the reverse 3 times, O(N) + O(K) + O(N-K) = O(2N) = O(N)
+// Time : O(n) Loops through array one time
 // Space : O(1) Does everything in place
-public class RotateArray {
+public class ReverseArray {
 
     private static final int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    // Reverse an Array
     static void main(String[] args) {
-        // Declare K here
-        // K = rotation distance
-        int k = 3;
         int n = nums.length;
-        k = k % n; // Normalizing K for out of bounds scenarios
-
         System.out.println("Before:");
         IO.println(Arrays.toString(nums));
 
@@ -24,21 +20,7 @@ public class RotateArray {
         reverse(nums, 0, n - 1);
         IO.println(String.format("After reversing whole array"));
         IO.println(Arrays.toString(nums));// Reverse Whole Array
-
-        // #2 Reverse first portion to k-1
-        // Now rotate the displaced elements at the front portion of k once more to get them back to correct order
-        reverse(nums, 0, k - 1);
-        IO.println(String.format("After reversing 0 to %d - 1", k));
-        IO.println(Arrays.toString(nums));
-
-        // #3 Reverse second portion of k to end of the array
-        // Now rotate the displace elements at the back portion of k once more to get them back to correct order
-        reverse(nums, k, n-1);
-        IO.println(String.format("After reversing %d to n - 1", k));
-        IO.println(Arrays.toString(nums));
     }
-
-    // Reverse an Array
     private static void reverse(int[] nums, int left, int right) {
         while (left < right) {
             swap(nums, left, right);
